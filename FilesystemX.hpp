0f9f3c930a24ffbae8e26b445c462f3ddaf63363
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-10-02 15:44:23 +0300

    Fix win32 file name and fix #includes

--- FilesystemX.hpp
+++ FilesystemX.hpp
@@ -1,10 +1,10 @@
 #if defined(NXDK) || defined(_WIN32)
 //#pragma message "Using nxdk/win32 version of FilesystemX"
-#include "filesystem_xbx.hpp"
+#include "backends/win32.hpp"
 #elif __cplusplus > 201600L
 //#pragma message "Using C++17 crossplatform version of FilesystemX"
 #define CPP17
-#include "filesystem_std.hpp"
+#include "backends/std_filesystem.hpp"
 #elif __cplusplus > 201302L
 //#pragma message "Using C++14 crossplatform version of FilesystemX"
 #define CPP14
@@ -13,5 +13,5 @@
 
 #else
 //#pragma message "Using C++98 POSIX version of FilesystemX"
-#include "filesystem_posix.hpp"
+#include "backends/posix.hpp"
 #endif
